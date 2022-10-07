c5a671f321cb1b30ac77951a7514ac4fb80b3772
Guilherme Bernal <82238770+Guilherme-Bernal@users.noreply.github.com>
2022-10-07 09:49:12 -0300

    correct bugs

--- robo.java
+++ robo.java
@@ -1,15 +1,3 @@
-/* 
-    Programação Orientada a Objetos
-    Professor : Luciano Freire
- -------------------------- Trabalho AC1 ( Desafio Robo fazendeiro ) -  TIN 1 --------------------------
- #  Aluno 1 : Douglas Braz Machado  - RA : 210034;
- #  Aluno 2 : Guilherme Savassa Bernal - RA : 210029;
- #  Aluno 3 : Gustavo Luciano Rossi Teixeira - RA : 210333.
-  
-  
-  
-*/
-
 import java.util.Scanner;
 
 public class robo {
@@ -18,17 +6,16 @@ public class robo {
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
         int y = in.nextInt();
-        int z = in.nextInt(); // G Rossi - O velho
+        int z = in.nextInt(); 
         int pos = 1;
         int local = 0;
 
         for (int i = 0; i < y; ++i) {
             if (pos == z) {
                 local++;
-            }
+            }S
 
-            pos += in.nextInt(); // Douglas - O pegador de casada
-
+            pos += in.nextInt();
             if (pos > x) {
                 pos = 1;
             } else if (pos == 0) {
@@ -40,6 +27,6 @@ public class robo {
         }
 
         System.out.println(local);
-        in.close(); // Guilherme - Pegador de Fuzil
-    }
+        in.close(); 
+            }
 }
